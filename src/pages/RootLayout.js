196ad524dc82c12e130bd28ea5be45aaf6b264f5
vtarun <vivektarun89@gmail.com>
2025-01-10 00:00:59 +0530

    Add Sidebar navigation with active link styling and Shopware icon

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -23,11 +23,11 @@ const RootLayout = () => {
         </div>
         {activeMenu ?
           (<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white"><Sidebar /></div>) :
-          (<div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>)
+          (<div className="w-0 dark:bg-secondary-dark-bg"><Sidebar /></div>)
         }
-        <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+        <div className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
           <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'> 
-          <Navbar />
+            <Navbar />
           </div>
         </div>
       </div>
