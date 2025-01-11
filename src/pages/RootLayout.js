c0e2eb5dd193d91cf6df5bbbf13d5d830d119b2c
vtarun <vivektarun89@gmail.com>
2025-01-12 00:35:37 +0530

    Create ecommerce component with earning data

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -32,10 +32,10 @@ const RootLayout = () => {
         <div className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
           <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'> 
             <Navbar />
-          </div>         
-        </div>
-        <Outlet/>
-      </div>      
+          </div>                 
+          <Outlet/>
+        </div> 
+      </div>     
     </div>
   )
 }
