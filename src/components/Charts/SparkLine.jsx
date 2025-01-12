3153536aad2ddd66564cd2f042ef9b9a2281178a
vtarun <vivektarun89@gmail.com>
2025-01-12 21:45:25 +0530

    Finish Revenue section in eCommerce component

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
+const SparkLine = ({id, currentColor, height, type, width, data, color }) => {
   return (
     <div>
-      SparkLine
+      <SparklineComponent 
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valuetype="Numeric"
+        fill={color}
+        dataSource={data}
+        border={{color: currentColor, width: 2}}
+        xName='x'
+        yName='y'
+        type={type}
+        tooltipSettings={{visible: true, format: '${x} : data ${y}',
+          trackLineSettings: {visible: true}
+        }}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
     </div>
   )
 }
