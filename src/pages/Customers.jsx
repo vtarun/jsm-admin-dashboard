a59bc093278d4ad983dc40a6179b5190089da1de
vtarun <vivektarun89@gmail.com>
2025-01-13 00:13:03 +0530

    Finish Customer and Employee component

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { 
+  GridComponent, 
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+  Search
+} from '@syncfusion/ej2-react-grids';
+
+import {
+  customersData,
+  customersGrid
+} from '../data/dummy';
+
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>
-      Customers
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Customers" category="Page" />
+      <GridComponent 
+        id="gridcomp"
+        dataSource={customersData}
+        allowPaging
+        allowSorting={true}
+        toolbar={['Delete']}
+        editSettings={{allowDeleting: true, allowEditing: true}}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[ Page, Search, Sort, Selection, Edit, Filter ,Toolbar]} />
+      </GridComponent>
     </div>
   )
 }
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Sort, Search, Inject, Toolbar} from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+
+import { Header } from '../components';
 
 const Employees = () => {
+  const sortSettings = { columns: [
+      { field: 'Designation' }
+  ] };
+  
+  
   return (
-    <div>
-      Employees
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Employees" category="Page" />
+      <GridComponent 
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting={true}
+        sortSettings={sortSettings}
+        toolbar={['Search']}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[ Page, Search, Sort, Toolbar]} />
+      </GridComponent>
     </div>
   )
 }
