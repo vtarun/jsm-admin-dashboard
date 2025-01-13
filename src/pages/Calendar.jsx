dfe15c66a78dc6c5ad1845e3983fec2bba38a2e0
vtarun <vivektarun89@gmail.com>
2025-01-13 15:57:26 +0530

    Finish calender component

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { 
   ScheduleComponent,
-  ViewsDirective,
-  ViewDirective,
   Day,
   Week,
   WorkWeek,
@@ -13,8 +11,6 @@ import {
   DragAndDrop 
 } from '@syncfusion/ej2-react-schedule';
 
-import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
-
 import { scheduleData } from '../data/dummy';
 import { Header } from '../components';
 
@@ -22,7 +18,11 @@ const Calendar = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
       <Header title='Calendar' category="App"/>
-      <ScheduleComponent>
+      <ScheduleComponent 
+        height="650px" 
+        eventSettings={{ dataSource: scheduleData }} 
+        selectedDate={new Date(2021, 0, 10)}
+      >
         <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
       </ScheduleComponent>
     </div>
