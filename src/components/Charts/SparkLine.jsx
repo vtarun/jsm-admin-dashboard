8b1bd6195a32baa7c6fbc1b44fc675dc00ced792
vtarun <vivektarun89@gmail.com>
2025-01-15 23:01:05 +0530

    Finish Area chart page

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = ({id, currentColor, height, type, width, data, color }) => {
-  return (
-    <div>
-      <SparklineComponent 
-        id={id}
-        height={height}
-        width={width}
-        lineWidth={1}
-        valuetype="Numeric"
-        fill={color}
-        dataSource={data}
-        border={{color: currentColor, width: 2}}
-        xName='x'
-        yName='y'
-        type={type}
-        tooltipSettings={{visible: true, format: '${x} : data ${y}',
-          trackLineSettings: {visible: true}
-        }}
-      >
-        <Inject services={[SparklineTooltip]} />
-      </SparklineComponent>
-    </div>
-  )
+class SparkLine extends React.PureComponent{
+  render() {
+  const {id, currentColor, height, type, width, data, color } = this.props;
+    return (      
+        <SparklineComponent 
+          id={id}
+          height={height}
+          width={width}
+          lineWidth={1}
+          valuetype="Numeric"
+          fill={color}
+          dataSource={data}
+          border={{color: currentColor, width: 2}}
+          xName='x'
+          yName='y'
+          type={type}
+          tooltipSettings={{visible: true, format: '${x} : data ${y}',
+            trackLineSettings: {visible: true}
+          }}
+        >
+          <Inject services={[SparklineTooltip]} />
+        </SparklineComponent>      
+    )
+  }
 }
 
 export default SparkLine
--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -30,7 +30,7 @@ const RootLayout = () => {
           (<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white"><Sidebar /></div>) :
           (<div className="w-0 dark:bg-secondary-dark-bg"><Sidebar /></div>)
         }
-        <div className={`dark:bg-main-dark-bg bg-main-dark-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+        <div className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
           <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'> 
             <Navbar />
           </div>
