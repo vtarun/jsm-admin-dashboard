8e54053dc74e8b4bada00089b404dc37dac96dc2
vtarun <vivektarun89@gmail.com>
2025-01-18 00:08:49 +0530

    Optimise Pyramid

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from 'react';
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  Inject, 
+  AccumulationLegend,
+  PyramidSeries,
+  AccumulationTooltip,
+  AccumulationDataLabel
+} from'@syncfusion/ej2-react-charts';
+
+import { PyramidData } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Pyramid = () => {
+  const { currentMode } = useStateContext();
+  const datalabel = { visible: true, position: 'Inside', name: 'text' };
   return (
-    <div>
-      Pyramid
-    </div>
+    <AccumulationChartComponent 
+      id='pyramid-chart' 
+      tooltip={{ enable: true }}
+      title='Food Comparison Chart'
+      legendSettings={{position: 'Bottom', background : 'white'}}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >      
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          dataSource={PyramidData}
+          xName='x'
+          yName='y'
+          type='Pyramid'
+          width='60%' 
+          height='80%'           
+          dataLabel={datalabel}
+          gapRatio={0.03}
+          name="Food"
+        />
+      </AccumulationSeriesCollectionDirective>
+      <Inject services={[AccumulationLegend, PyramidSeries, AccumulationTooltip, AccumulationDataLabel]} />
+    </AccumulationChartComponent>
   )
 }
 
-export default Pyramid
-Pyramid
+export default Pyramid;
