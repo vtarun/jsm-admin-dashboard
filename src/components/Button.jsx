2db040875ca730315f37c5a190f92b96a89441ab
vtarun <vivektarun89@gmail.com>
2025-01-17 19:26:52 +0530

    Finish notification popup

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Button = ({bgColor, color, size, text, borderRadius}) => {
+const Button = ({bgColor, color, size, text, borderRadius, className}) => {
   return (
     <button 
       type="button" 
       style={{backgroundColor: bgColor, color, borderRadius}}
-      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      className={`text-${size} p-2 hover:drop-shadow-xl ${className}`}
     >
       {text}
     </button>
